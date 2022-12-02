05e8a0d66798ce9eaefd42071acf834b0f318bfc
nestorvillap <nestorvillap@gmail.com>
2022-12-02 19:27:51 +0100

    Se han hecho mejoras en el codigo en general, y se ha implementado parsearDireccion, ademas de desarrollarse un poco la parte de accesos a memoria.

--- MEMsym.c
+++ MEMsym.c
@@ -1,18 +1,38 @@
-//bus 12
-//cache 8 lineas correct direct 16 bytes por linea
+//tamaño direccion: 12bits
 
-//palabra 4
-//linea 3
-//etiqueta 5
+//linea: 3bits
+//etiqueta: 5bits
+//palabra: 4bits
 
+// RAM
+
+//bloques: 256
+//tamaño: 4096 bytes
+
+//CACHE correct direct
+
+//tamaño cache = 128bytes
+//lineas: 8
+//tamaño de linea: 16bytes
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define TAM_LINE 3
+#define TAM_LINE 16
 #define NUM_FILAS 3
-
+//
+//
+//------------------ 16 bytes = 128 bits ETQ= 5bits data 
+//
+//
+//
+//
+//
+//
+//
+//
+//
 typedef struct {
     unsigned char ETQ;
     unsigned char Data[TAM_LINE];
@@ -29,41 +49,53 @@ int main (int argc, char* argv[]){
 
     int globaltime=0,numfallos=0;
 
-    T_CACHE_LINE linea[16];
+    T_CACHE_LINE tbl[16];
 
     unsigned char Simul_RAM[4096];
-
+    
     char* cadenaTemp=malloc(1000 * sizeof(char*)); 
     char texto[100];
 
+    unsigned int addr;
+    int ETQ,palabra,linea,bloque;
+
     for(int i=0;i<16;i++){
-        linea[i].ETQ=0xFF;
+        tbl[i].ETQ=0xFF;
         for(int j=0;j<TAM_LINE;j++){
-            linea[i].Data[j]=(unsigned char)0x23f;
+            tbl[i].Data[j]=(unsigned char)0x23f;
         }
     }
 
-    for (int i = 1; (leerLinea("CONTENTS_RAM.bin",i,cadenaTemp))==1 ; i++)
+    for (int i = 1; (leerLinea("CONTENTS_RAM.bin",i,cadenaTemp)) == 1 ; i++)
     {
         strcat(Simul_RAM,cadenaTemp);    
     }
-    
-    printf("%s\n",Simul_RAM);
 
-    for (int i = 1; (leerLinea("accesos_memoria.txt",i,cadenaTemp))==1 ; i++)
+    for (int i = 1; (leerLinea("accesos_memoria.txt",i,cadenaTemp)) == 1 ; i++)
     {
-        //linea == direccion
-        //obtener num linea
-        //if (numlinea!=label)
-        // {
-        //     numfallos++;
-        // }
-        //printf("T: %d, Acierto de CACHE, ADDR %04X Label%X linea %02X palabra %02X DATO %02X ");
+
+        // TENGO UN DIRECCION EN HEX
+        // ETQ/LINEA/PALABRA
+        ParsearDireccion(strtol(cadenaTemp, NULL, 16),&ETQ,&palabra,&linea,&bloque);
+        if (tbl[linea].ETQ!=ETQ)
+        {
+            numfallos++;
+            //Copiar el bloque de la ram
+            
+            printf("se está cargando el bloque X en la líneaY\n");
+            //actulizar ETQ Y DATOS
+            tbl[linea].ETQ=ETQ;
+            for (int i = 0; i < TAM_LINE; ++i)
+            {
+                texto[i]=Simul_RAM[bloque*TAM_LINE+i];
+            }
+        }
+        printf("T: %d, Fallo de CACHE %d, ADDR %04X Label %X linea %02X palabra %02X bloque %02X\n",globaltime,addr,ETQ,linea,palabra,bloque);
         //texto[i]=caracterLeido;
         //VOLCAR CONTENIDO DE LA CACHE==HACER LA TABLA
         //sleep(1);
     }
-    
+    //printf("T: %d, Acierto de CACHE, ADDR %04X Label%X linea %02X palabra %02X DATO %02X ",globaltime,addr,ETQ,linea,palabra,dato);
     return  0;
 }
 
@@ -93,6 +125,12 @@ int leerLinea(char nombre[], int linea,char* cadenaTemp){
 
     return 1;
 }
+
 void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]);
 void VolcarCACHE(T_CACHE_LINE *tbl);
-void ParsearDireccion(unsigned int addr, int *ETQ, int*palabra, int *linea, int *bloque);
+void ParsearDireccion(unsigned int addr, int *ETQ, int*palabra, int *linea, int *bloque){
+    *palabra = addr & 0b1111;
+    *bloque = addr >> 4;
+    *linea = (*bloque & 0b111);
+    *ETQ = (*bloque & 0b11111000)>>3;
+};
