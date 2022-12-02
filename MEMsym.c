b224b312018acd9f13c655b669bfa374f9d7192d
carmonalanzasalvaro <carmonalanzasalvaro@gmail.com>
2022-12-02 22:10:51 +0100

    2/12/2022

--- MEMsym.c
+++ MEMsym.c
@@ -48,9 +48,11 @@ int main (int argc, char* argv[]){
 
     //DECLARAMOS E INICIALIZAMOS TODO
 
-    int globaltime=0,numfallos=0;
+    int globaltime=0,numfallos=0,numaccesos=0;
 
     T_CACHE_LINE tbl[16];
+    
+    LimpiarCACHE(tbl);
 
     unsigned char Simul_RAM[4096];
     
@@ -60,16 +62,11 @@ int main (int argc, char* argv[]){
     unsigned int addr;
     int ETQ,palabra,linea,bloque;
 
-    for(int i=0;i<16;i++){
-        tbl[i].ETQ=0xFF;
-        for(int j=0;j<TAM_LINE;j++){
-            tbl[i].Data[j]=0x23;
-        }
-    }
+   
 
     for (int i = 1; (leerLinea("CONTENTS_RAM.bin",i,cadenaTemp)) == 1 ; i++)
     {
-        strcat(Simul_RAM,cadenaTemp);    
+        strcat(Simul_RAM,cadenaTemp);
     }
 
     for (int i = 1; (leerLinea("accesos_memoria.txt",i,cadenaTemp)) == 1 ; i++)
@@ -77,27 +74,36 @@ int main (int argc, char* argv[]){
 
         // TENGO UN DIRECCION EN HEX
         // ETQ/LINEA/PALABRA
-        ParsearDireccion(strtol(cadenaTemp, NULL, 16),&ETQ,&palabra,&linea,&bloque);
+        addr=strtol(cadenaTemp, NULL, 16);
+        ParsearDireccion(addr,&ETQ,&palabra,&linea,&bloque);
         if (tbl[linea].ETQ!=ETQ)
         {
             //Sumar fallo
             numfallos++;
             //Mensaje de fallo
             printf("T: %d, Fallo de CACHE %d, ADDR %04X Label %X linea %02X palabra %02X bloque %02X\n",globaltime,numfallos,addr,ETQ,linea,palabra,bloque);
+            //Sumar a global time
+            globaltime+=10;
             //Copiar el bloque de la ram
-            printf("se está cargando el bloque %02X en la línea %02X\n",bloque,linea);
+            printf("Se está cargando el bloque %02X en la línea %02X\n",bloque,linea);
             //actulizar ETQ Y DATOS
             tbl[linea].ETQ=ETQ;
-            for (int i = 0; i < TAM_LINE; ++i)
+            for (int j = 0; j < TAM_LINE; j++)
             {
-                tbl[linea].Data[i]=Simul_RAM[bloque*TAM_LINE+i];
+                tbl[linea].Data[j]=Simul_RAM[bloque*TAM_LINE+j];
             }
+
         }
-        //texto[i]=caracterLeido;
+        printf("T: %d, Acierto de CACHE, ADDR %04X Label %X linea %02X palabra %02X DATO %02X\n",globaltime,addr,ETQ,linea,palabra,tbl[linea].Data[palabra]);
+        //Copiar los caracteres en texto
+        texto[numaccesos]=tbl[linea].Data[palabra];
+        numaccesos++;
         VolcarCACHE(tbl);
         sleep(1);
     }
-    //printf("T: %d, Acierto de CACHE, ADDR %04X Label%X linea %02X palabra %02X DATO %02X ",globaltime,addr,ETQ,linea,palabra,dato);
+    texto[numaccesos+1]='\0';
+    printf("Accesos totales: %d, fallos: %d, tiempo medio: %.2f\n",numaccesos,numfallos,((float)globaltime/(float)(numaccesos+1)));
+    printf("Texto leido, \"%s\"\n",texto);
     return  0;
 }
 
@@ -115,25 +121,30 @@ int leerLinea(char nombre[], int linea,char* cadenaTemp){
 
     for (int i = 0; (i < linea); i++)
     {
+        fgets(cadenaTemp,4096,archivo);
         if (feof(archivo))
         {
             return -1;
         }
-
-        fgets(cadenaTemp,4096,archivo); 
-    } 
-
+    }
     fclose(archivo);
 
     return 1;
 }
 
-void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]);
+void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]){
+     for(int i=0;i<16;i++){
+        tbl[i].ETQ=0xFF;
+        for(int j=0;j<TAM_LINE;j++){
+            tbl[i].Data[j]=0x23;
+        }
+    }
+}
 void VolcarCACHE(T_CACHE_LINE *tbl){
     for (int i = 0; i < 8; i++)
     {
         printf("ETQ:FF DATA ",tbl[i].ETQ);
-        for (int j = 0; j < 16; j++)
+        for (int j = 15; j >= 0; j--)
         {
             printf("%X ", tbl[i].Data[j]);
         }
